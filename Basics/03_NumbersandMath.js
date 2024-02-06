a62392b988a75a11149df667987dc29843cc10ff
QuasimKhan <quasimkhanpersonal@gmail.com>
2024-02-06 16:06:28 +0530

    Updated

---
+++ Basics/03_NumbersandMath.js
@@ -0,0 +1,38 @@
+// In this program I will learn about number and math in JS 
+
+// const score = 4000000
+// console.log(score);
+
+// const age = new Number(18)
+// console.log(age)
+
+// Both ways are same but age will surely be a number 
+
+// lets use some properties and method of number 
+
+// console.log(score.toString())       //It converts it tpye into string but main score variable remaine same as number. we know it make a copy of score variable and change in copy not in original
+//We can also use All string properties and methods after converting to string
+
+
+// console.log(score.toFixed(2))       //It fixed upto 2 after decimal Output:400.00
+
+// console.log(score.toExponential(0))         //covert into exponential
+
+// console.log(score.toLocaleString('en-IN'))        // return a string with indain system commas
+
+
+
+
+// ---------------+++++++++++++-------------++++++++++++---------
+
+            // Maths
+
+const score = 2000.1213244;
+
+// console.log(Math.round(score))      //It gives round off vaule
+
+// console.log(Math.cbrt(score))       //It return cube root 
+
+// console.log(Math.floor(score))      //It only return integers and ignore the float value
+
+
